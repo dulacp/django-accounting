303e0ed5b507a29f6893047dcca1c1cfa1647f94
Pierre Dulac <dulacpier@gmail.com>
2014-10-12 01:06:35 +0200

    Fix the nav active templatetag

--- accounting/libs/templatetags/nav.py
+++ accounting/libs/templatetags/nav.py
@@ -7,7 +7,12 @@ register = template.Library()
 
 
 @register.simple_tag
-def active(request, pattern):
+def active(request, pattern, exact_match=False):
+    if exact_match:
+        if not pattern.startswith('^'):
+            pattern = '^' + pattern
+        if not pattern.endswith('$'):
+            pattern = pattern + '$'
     if hasattr(request, 'path') and re.search(pattern, request.path):
         return 'active'
     return ''
