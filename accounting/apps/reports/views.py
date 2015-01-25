08eb24251f040966026c71503d1c25353d6b34f3
Pierre Dulac <dulacpier@gmail.com>
2015-01-25 19:06:57 +0100

    Add the period form to the tax report

--- accounting/apps/reports/views.py
+++ accounting/apps/reports/views.py
@@ -24,82 +24,6 @@ from .wrappers import (
     InvoiceDetailsReport)
 
 
-class ReportListView(generic.TemplateView):
-    template_name = "reports/report_list.html"
-
-
-class SettingsListView(generic.TemplateView):
-    template_name = "reports/settings_list.html"
-
-
-class GenericSettingsMixin(object):
-
-    def get_object(self):
-        orga = organization_manager.get_selected_organization(self.request)
-        try:
-            settings = self.model.objects.get(organization=orga)
-        except self.model.DoesNotExist:
-            settings = self.model.objects.create(organization=orga)
-        return settings
-
-    def get_success_url(self):
-        return reverse("reports:settings-list")
-
-
-class BusinessSettingsUpdateView(GenericSettingsMixin,
-                                  generic.UpdateView):
-    template_name = "reports/financial_settings_update.html"
-    model = BusinessSettings
-    form_class = BusinessSettingsForm
-
-
-class FinancialSettingsUpdateView(GenericSettingsMixin,
-                                  generic.UpdateView):
-    template_name = "reports/financial_settings_update.html"
-    model = FinancialSettings
-    form_class = FinancialSettingsForm
-
-
-class PayRunSettingsUpdateView(GenericSettingsMixin,
-                               generic.UpdateView):
-    template_name = "reports/payrun_settings_update.html"
-    model = PayRunSettings
-    form_class = PayRunSettingsForm
-
-
-class TaxReportView(generic.TemplateView):
-    template_name = "reports/tax_report.html"
-
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        orga = organization_manager.get_selected_organization(self.request)
-        report = TaxReport(orga,
-                           start=date(2014, 1, 1),
-                           end=date(2014, 12, 1))
-        report.generate()
-        ctx['tax_summaries'] = report.tax_summaries.values()
-        return ctx
-
-
-class ProfitAndLossReportView(generic.TemplateView):
-    template_name = "reports/profit_and_loss_report.html"
-
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        orga = organization_manager.get_selected_organization(self.request)
-
-        # currrent quarter
-        now = timezone.now()
-        start = date(year=now.year, month=(now.month - ((now.month-1) % 3)), day=1)
-        end = start + relativedelta(months=3)
-
-        report = ProfitAndLossReport(orga, start=start, end=end)
-        report.generate()
-        ctx['summaries'] = report.summaries
-        ctx['total_summary'] = report.total_summary
-        return ctx
-
-
 class TimePeriodFormMixin(object):
 
     period = None
@@ -143,6 +67,84 @@ class TimePeriodFormMixin(object):
         return ctx
 
 
+class ReportListView(generic.TemplateView):
+    template_name = "reports/report_list.html"
+
+
+class SettingsListView(generic.TemplateView):
+    template_name = "reports/settings_list.html"
+
+
+class GenericSettingsMixin(object):
+
+    def get_object(self):
+        orga = organization_manager.get_selected_organization(self.request)
+        try:
+            settings = self.model.objects.get(organization=orga)
+        except self.model.DoesNotExist:
+            settings = self.model.objects.create(organization=orga)
+        return settings
+
+    def get_success_url(self):
+        return reverse("reports:settings-list")
+
+
+class BusinessSettingsUpdateView(GenericSettingsMixin,
+                                  generic.UpdateView):
+    template_name = "reports/financial_settings_update.html"
+    model = BusinessSettings
+    form_class = BusinessSettingsForm
+
+
+class FinancialSettingsUpdateView(GenericSettingsMixin,
+                                  generic.UpdateView):
+    template_name = "reports/financial_settings_update.html"
+    model = FinancialSettings
+    form_class = FinancialSettingsForm
+
+
+class PayRunSettingsUpdateView(GenericSettingsMixin,
+                               generic.UpdateView):
+    template_name = "reports/payrun_settings_update.html"
+    model = PayRunSettings
+    form_class = PayRunSettingsForm
+
+
+class TaxReportView(TimePeriodFormMixin,
+                    generic.FormView):
+    template_name = "reports/tax_report.html"
+    form_class = TimePeriodForm
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        orga = organization_manager.get_selected_organization(self.request)
+        report = TaxReport(orga,
+                           start=self.period.start,
+                           end=self.period.end)
+        report.generate()
+        ctx['tax_summaries'] = report.tax_summaries.values()
+        return ctx
+
+
+class ProfitAndLossReportView(generic.TemplateView):
+    template_name = "reports/profit_and_loss_report.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        orga = organization_manager.get_selected_organization(self.request)
+
+        # currrent quarter
+        now = timezone.now()
+        start = date(year=now.year, month=(now.month - ((now.month-1) % 3)), day=1)
+        end = start + relativedelta(months=3)
+
+        report = ProfitAndLossReport(orga, start=start, end=end)
+        report.generate()
+        ctx['summaries'] = report.summaries
+        ctx['total_summary'] = report.total_summary
+        return ctx
+
+
 class PayRunReportView(TimePeriodFormMixin,
                        generic.FormView):
     template_name = "reports/pay_run_report.html"
@@ -152,7 +154,9 @@ class PayRunReportView(TimePeriodFormMixin,
         ctx = super().get_context_data(**kwargs)
         orga = organization_manager.get_selected_organization(self.request)
 
-        report = PayRunReport(orga, start=self.period.start, end=self.period.end)
+        report = PayRunReport(orga,
+                              start=self.period.start,
+                              end=self.period.end)
         report.generate()
         ctx['summaries'] = report.summaries.values()
         ctx['total_payroll_taxes'] = report.total_payroll_taxes
