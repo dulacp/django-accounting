c7a1805d14662ec1579c7bd519dddcba4099014a
Pierre Dulac <dulacpier@gmail.com>
2015-01-28 23:29:15 +0100

    Add a missing admin model for the employee class

--- accounting/apps/people/admin.py
+++ accounting/apps/people/admin.py
@@ -6,3 +6,8 @@ from . import models
 @admin.register(models.Client)
 class ClientAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(models.Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
