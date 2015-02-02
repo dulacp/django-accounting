160a045e055439bca94e6b32ee6df7ab3fa7aaae
Pierre Dulac <dulacpier@gmail.com>
2015-02-02 16:56:57 +0100

    Fix the user organization context processors

--- accounting/apps/books/context_processors.py
+++ accounting/apps/books/context_processors.py
@@ -6,8 +6,16 @@ def organizations(request):
     """
     Add some generally useful metadata to the template context
     """
+    # selected organization
     orga = organization_manager.get_selected_organization(request)
+
+    # all user authorized organizations
+    if not request.user or not request.user.is_authenticated():
+        user_organizations = None
+    else:
+        user_organizations = request.user.organizations.all()
+
     return {
-        'all_organizations': Organization.objects.all(),
+        'user_organizations': user_organizations,
         'selected_organization': orga,
     }
