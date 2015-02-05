cd081f9ee5677b1e9031e1edfa8e4ef735017f24
Pierre Dulac <dulacpier@gmail.com>
2015-02-05 17:08:15 +0100

    Bump version 0.1.1

--- accounting/__init__.py
+++ accounting/__init__.py
@@ -3,7 +3,7 @@ import os
 # Use 'final' as the 4th element to indicate
 # a full release
 
-VERSION = (0, 1, 0, 'alpha', 1)
+VERSION = (0, 1, 1)
 
 
 def get_short_version():
@@ -15,10 +15,6 @@ def get_version():
     # Append 3rd digit if > 0
     if VERSION[2]:
         version = '%s.%s' % (version, VERSION[2])
-    elif VERSION[3] != 'final':
-        version = '%s %s' % (version, VERSION[3])
-        if len(VERSION) == 5:
-            version = '%s %s' % (version, VERSION[4])
     return version
 
 
